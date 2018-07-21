20b5c1cfb728a8e0b32b3be9c295b73c1944ea26
Abraham Alcaina <abraham@weareadaptive.com>
2018-07-21 14:55:00 +0200

    test  add more test

--- test/User/ESSpec.hs
+++ test/User/ESSpec.hs
@@ -17,13 +17,22 @@ commandAction cmd = \_ s -> do
 newName = "Pablito"
 
 spec :: Spec
-spec = describe "Test for in memory Event Sourcing" $ do
-    it "New store should contains a init user" $ do
-        store <- newInMemoryStore
-        user <- sPoll store nil
-        user `shouldBe` initUser
-    it "Update user" $ do
-        store <- newInMemoryStore
-        sUpdate store (commandAction (RenameUser newName)) nil
-        user <- sPoll store nil
-        name user `shouldBe` newName
+spec = describe "Test in memory Event Sourcing" $ do
+    context "New store" $ do
+        it "should contains a init user" $ do
+            store <- newInMemoryStore
+            user <- sPoll store nil
+            user `shouldBe` initUser
+        context "Add a new user" $
+            it "should had the new user" $ do
+                store <- newInMemoryStore
+                uuid <- uuidNextRandom
+                sUpdate store (commandAction (CreateUser uuid newName)) uuid
+                user <- sPoll store uuid
+                name user `shouldBe` newName
+    context "Update user" $
+        it "should change the name" $ do
+            store <- newInMemoryStore
+            sUpdate store (commandAction (RenameUser newName)) nil
+            user <- sPoll store nil
+            name user `shouldBe` newName

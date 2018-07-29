abaa6c5325708d0d7c87498a7fab5d1d2348a99e
Abraham Alcaina <abraham@weareadaptive.com>
2018-07-29 22:49:40 +0200

    chore move setupPool to DB file

--- app/Main.hs
+++ app/Main.hs
@@ -2,19 +2,14 @@
 
 module Main where
 
-import App
 import Control.Lens
-import Control.Monad.IO.Class
-import Control.Monad.Logger    (NoLoggingT(..), runNoLoggingT)
-import Data.Maybe
-import Data.Text               (pack)
-import Data.Text.Lazy          (Text)
-import Database.Persist.Sqlite
-import Eventful.Store.Sqlite
+import Data.Maybe         (fromMaybe)
 import System.Environment
-import Users                   (usersRoute)
-import Web.Scotty.Trans        as S
+import Web.Scotty.Trans
 
+import App
+import DB    (setupPool)
+import Users (usersRoute)
 
 main :: IO ()
 main = do
@@ -22,7 +17,6 @@ main = do
   let port = cfg ^. (appWebConfig . webPort)
   scottyT port (runApp cfg)  app
 
-app :: ScottyT Text AppM ()
 app = do
   usersRoute
 
@@ -36,9 +30,3 @@ getConfig = do
     webConfig = WebConfig (read(fromMaybe "8080" portM))
   pool <- setupPool (dbConfig ^. fileDB) (dbConfig ^. poolSize)
   return $ AppConfig dbConfig webConfig pool
-
-setupPool :: String -> Int -> IO ConnectionPool
-setupPool fileDB poolSize =
-    runNoLoggingT $ withSqlitePool (pack fileDB)  poolSize $ \pool -> NoLoggingT $ do
-    initializeSqliteEventStore defaultSqlEventStoreConfig pool
-    return pool
